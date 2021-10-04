7d5c7cf98f2b4f3b7ea7d4faa962ded493e577bb
MARQUES MQ <luan1083@hotmail.com>
2021-10-03 22:44:03 -0300

    Cor background

--- index.php
+++ index.php
@@ -6,17 +6,19 @@
 <html lang="pt-br">
     <head>
         <!-- CSS -->
+    <link href="style.css" rel="stylesheet"/>    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
         <title>+Malas</title>
     </head>
 
-    <body>
+    <body class="body-gradient">
         <!-- Container --> 
         <div class="container">
         <br>
 
         <!-- Lista de produtos -->
-        <table class="table">
+        <div class="table-responsive-sm">
+        <table class="table table-striped">
         <thead class="table-dark">
             <tr>
                 <th scope="col">Id</th>
@@ -82,6 +84,7 @@
     
         </tbody>    
         </table>
+        </div>
         <!-- Final lista de produtos -->
         
         </div> 
--- pages/navbar_global.php
+++ pages/navbar_global.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
+<link href="../style.css" rel="stylesheet"/>    
 <html>
-        <nav class="navbar navbar-dark bg-dark">
+        <nav class="navbar navbar-light" style="background-color: #2c3e50;">
             <div class="container-fluid">
                 <a href="#" class="navbar-brand">+Malas</a>
             </div>
